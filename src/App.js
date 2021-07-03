61b37499e72fd871b40ab62a0659397d314881af
bangnokia <bangnokia@gmail.com>
2021-07-03 16:03:55 +0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import appConfig from './config/app';
+import { app } from '@tauri-apps/api';
+import { Command } from '@tauri-apps/api/shell';
 
 function App() {
+  const [code, setCode] = useState("User::where('name', 'daudau')->first()")
+  const [output, setOutput] = useState('con meo to');
+
+  const command = new Command('ls -la');
+
+  command.stdout.on('data', line => setOutput(line));
+  command.spawn();
+
+  console.log('name daudau')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="min-h-screen grid grid-cols-2 bg-gray-500 divide-x-2 divide-gray-800">
+      <div class="p-5">
+        <textarea
+          value={code}
+          onChange={e => setCode(e.target.value)}
+          className="w-full h-full bg-gray-500 focus:outline-none" />
+      </div>
+
+      <div className="result p-5">
+        {output}
+      </div>
+    </div >
   );
 }
 
---
+++ src/config/app.js
@@ -0,0 +1,6 @@
+const appConfig = {
+    mode: 'local',
+    workingDirectory: '/Users/daudau/Code/laravel8'
+};
+
+export default appConfig;
