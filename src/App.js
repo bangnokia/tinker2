cc02a6d208a39d33fb89e4656e39c7b9b6adff3b
bangnokia <bangnokia@gmail.com>
2021-07-08 01:18:52 +0700

    wip

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import appConfig from './config/app';
 import { app } from '@tauri-apps/api';
 import { Command } from '@tauri-apps/api/shell';
 
+async function execute(...args) {
+  const output = await new Command(...args).execute();
+
+  return output;
+}
+
 function App() {
-  const [code, setCode] = useState("User::where('name', 'daudau')->first()")
+  const [code, setCode] = useState("ls")
   const [output, setOutput] = useState('con meo to');
 
-  const command = new Command('ls -la');
-
-  command.stdout.on('data', line => setOutput(line));
-  command.spawn();
-
-  console.log('name daudau')
+  const runCode = function () {
+    execute(code)
+      .then(result => setOutput(result.stdout))
+      .catch(error => console.log('error', error))
+  }
+  //setOutput(result);
 
   return (
     <div className="min-h-screen grid grid-cols-2 bg-gray-500 divide-x-2 divide-gray-800">
       <div class="p-5">
+        <button onClick={runCode} className="border bg-red-500">Execute</button>
         <textarea
           value={code}
           onChange={e => setCode(e.target.value)}
@@ -26,7 +33,7 @@ function App() {
       <div className="result p-5">
         {output}
       </div>
-    </div >
+    </div>
   );
 }
 
