e0c87fd7fa8487f3f631d0f484da3e527cb6c4eb
bangnokia <bangnokia@gmail.com>
2021-07-22 03:16:39 +0700

    add support copy paste

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,40 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 fn main() {
+  let menu = make_menu();
+
   tauri::Builder::default()
+    .menu(menu)
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+}
+
+fn make_menu() -> Menu {
+  let main_submenu = Submenu::new(
+    "Tinker 2",
+    Menu::new()
+      .add_native_item(MenuItem::About("About Tinker 2".to_string()))
+      .add_native_item(MenuItem::EnterFullScreen)
+      .add_native_item(MenuItem::Quit),
+  );
+
+  let edit_submenu = Submenu::new(
+    "Edit",
+    Menu::new()
+      .add_native_item(MenuItem::Undo)
+      .add_native_item(MenuItem::Redo)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Cut)
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Paste)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::SelectAll),
+  );
+
+  Menu::new()
+    .add_submenu(main_submenu)
+    .add_submenu(edit_submenu)
 }
