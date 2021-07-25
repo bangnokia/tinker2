729538b5a822360ad150646817fc135a7377e1b5
bangnokia <bangnokia@gmail.com>
2021-07-26 01:02:00 +0700

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import StatusBar from './StatusBar';
 import Playground from './Playground';
 import Settings from './Settings';
