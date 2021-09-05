972866cd7745fb5cc0761cdb6f720de1703cff9b
bangnokia <bangnokia@gmail.com>
2021-09-06 03:26:44 +0700

    0.1.8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx,css}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
       extend: {
