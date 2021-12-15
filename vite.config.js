25c672baad947a0deb7a5ab8a2817c7b4c3f0731
bangnokia <bangnokia@gmail.com>
2021-12-15 20:39:13 +0700

    work in progress

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    build: {
+        outDir: 'build',
+        chunkSizeWarningLimit: 5000
+    },
+    plugins: [react()]
 })
